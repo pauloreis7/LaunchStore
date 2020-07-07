8c83e5aa52993fe9fc40432d1f852dfbda06c759
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-07-06 21:05:08 -0300

    Adição do campo de upload de imagens
    O campo de upload de imagens foi adicionada e suas formatações e regars de negócio iniciadas

--- public/scripts.js
+++ public/scripts.js
@@ -18,3 +18,60 @@ const Mask = {
         }).format(value/100)
     }
 }
+
+const PhotosUpload = {
+    uploadLimit: 6,
+    preview:  document.querySelector('#photos_preview'),
+
+
+    handleFileInput(event) {
+
+        const { files: filesList } = event.target
+
+        if(PhotosUpload.LimitValidations(event, filesList)) return
+        
+
+        Array.from(filesList).forEach( file => {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+                
+                const container = PhotosUpload.createCotainer(image)
+                
+                PhotosUpload.preview.appendChild(container)        
+                
+                const totalImages = document.querySelectorAll('.photo').length
+                document.querySelector('.images_title').textContent = `Imagens (${ totalImages })`
+            }
+
+            reader.readAsDataURL(file)
+        })
+    },
+
+    createCotainer(image) { 
+
+        const container = document.createElement("div")
+        container.classList.add('photo')
+        
+        container.onclick = () => alert("Cliquei!!")
+        container.appendChild(image)
+
+        return container
+    },
+
+    LimitValidations(event, filesList) {
+
+        const { uploadLimit } = PhotosUpload
+
+        if (filesList.length > uploadLimit) {
+            alert(`Selecione no máximo ${ uploadLimit } fotos!!`)
+
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    }
+}
