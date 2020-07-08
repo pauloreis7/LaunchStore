3be989d518fb8fe9910e54123f9d56de02f72e3d
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-07-07 21:08:42 -0300

    Aprimorando a função de upload de imagens
    A funcionalidade de upload de arquivos foi aprimorada com novas funcionalidades e regras de negócio

--- public/scripts.js
+++ public/scripts.js
@@ -20,34 +20,39 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    input: "",
     uploadLimit: 6,
-    preview:  document.querySelector('#photos_preview'),
-
+    preview: document.querySelector('#photos_preview'),
+    files: [],
 
     handleFileInput(event) {
 
         const { files: filesList } = event.target
-
-        if(PhotosUpload.LimitValidations(event, filesList)) return
+        PhotosUpload.input = event.target
         
+        if(PhotosUpload.LimitValidations(event)) return        
 
         Array.from(filesList).forEach( file => {
+            
+            PhotosUpload.files.push(file)
+
             const reader = new FileReader()
 
             reader.onload = () => {
                 const image = new Image()
                 image.src = String(reader.result)
-                
+
                 const container = PhotosUpload.createCotainer(image)
                 
-                PhotosUpload.preview.appendChild(container)        
-                
-                const totalImages = document.querySelectorAll('.photo').length
-                document.querySelector('.images_title').textContent = `Imagens (${ totalImages })`
+                PhotosUpload.preview.appendChild(container)
             }
 
             reader.readAsDataURL(file)
         })
+        
+        PhotosUpload.PhotosCount()
+
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
     },
 
     createCotainer(image) { 
@@ -55,17 +60,28 @@ const PhotosUpload = {
         const container = document.createElement("div")
         container.classList.add('photo')
         
-        container.onclick = () => alert("Cliquei!!")
         container.appendChild(image)
+
+        container.appendChild(PhotosUpload.RemoveButton())
 
         return container
     },
 
-    LimitValidations(event, filesList) {
+    RemoveButton() {
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "delete_forever"
 
-        const { uploadLimit } = PhotosUpload
+        button.onclick = PhotosUpload.RemovePhoto
 
-        if (filesList.length > uploadLimit) {
+        return button
+    },
+
+    LimitValidations(event) {
+
+        const { uploadLimit, input } = PhotosUpload
+
+        if (input.files.length > uploadLimit) {
             alert(`Selecione no máximo ${ uploadLimit } fotos!!`)
 
             event.preventDefault()
@@ -73,5 +89,33 @@ const PhotosUpload = {
         }
 
         return false
+    },
+
+    getAllFiles () {
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+        return dataTransfer.files
+
+    },
+
+    RemovePhoto (event) {
+        const photoContainer = event.target.parentNode // .photo
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoContainer)
+
+        PhotosUpload.files.splice(index, 1)
+        
+        PhotosUpload.PhotosCount()
+        
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+        photoContainer.remove()
+    },
+
+    PhotosCount () {
+        const totalImages = PhotosUpload.files.length
+        document.querySelector('.images_title').textContent = `Imagens (${ totalImages })`
     }
 }
